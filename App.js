74ae5e9b72a0c4db63e6fb1c7b4b133fcfab0e5f
Judith van Leersum <judithvleersum@gmail.com>
2022-08-24 17:05:03 +0200

    Added header button on welcome screen to logout through auth context

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { StatusBar } from "expo-status-bar";
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
+import IconButton from "./components/ui/IconButton";
 import { Colors } from "./constants/styles";
 import AuthContext, { AuthContextProvider } from "./store/auth-context";
 import { useContext } from "react";
@@ -27,6 +28,8 @@ function AuthStack() {
 }
 
 function AuthenticatedStack() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <Stack.Navigator
       screenOptions={{
@@ -35,7 +38,20 @@ function AuthenticatedStack() {
         contentStyle: { backgroundColor: Colors.primary100 },
       }}
     >
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{
+          headerRight: ({ tintColor }) => (
+            <IconButton
+              icon="exit-outline"
+              color={tintColor}
+              size={24}
+              onPress={authCtx.logout}
+            />
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 }
